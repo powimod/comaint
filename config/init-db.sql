0b0d241896df495956b1652fa20969fcb96a1e19
Dominique Parisot <powimod@gmail.com>
2023-12-02 09:18:40 +0000

    Create table, foreign keys and indexes in SQL script

--- config/init-db.sql
+++ config/init-db.sql
@@ -1,24 +1,112 @@
 -- Project {{project.name}} 
 -- Database schema version {{project.attributes.database_schema_version}}
--- Database initialization script
-{% for object in project.objects %}
+-- MySQL Database initialization script
 
+-- CREATE SCHEMA db_comaint;
+USE {{project.attributes.database_name}}
+-- CREATE USER 'comaint'@'localhost' IDENTIFIED BY 'g4m0-KauM1nt';
+-- GRANT ALL PRIVILEGES ON db_comaint.* TO 'comaint'@'localhost';
+
+
+{% for object in project.objects -%}
+	DROP TABLE IF EXISTS {{object.attributes.table_name}};
+{% endfor -%}
+
+--------------------------------------------------------------------------------
+--     Tables                                                                 --
+--------------------------------------------------------------------------------
+
+{% for object in project.objects %}
+--
+-- Table {{object.attributes.table_name}}
+--
 CREATE TABLE IF NOT EXISTS {{object.attributes.table_name}}(
 	{% for property in object.properties %}
-	{%- if property.type.name == 'id' -%}
-	{{property.name}} INTEGER NOT NULL auto_increment, 
-	{%- endif -%}
+		{%- if property.type.name == 'id' -%}
+			{{property.name}} INTEGER NOT NULL auto_increment, 
+		{%- endif -%}
 	{%- endfor -%}
 
+	{% for link in object.links %}
+	id{{link.name | capitalize}} INTEGER NOT NULL, 
+	{%- endfor -%}
+
+
 	{% for property in object.properties %}
-	{%- if property.mandatory -%}
-	{%-      assign mandatory = "NOT NULL" -%}
-	{%- else -%}
-	{%-      assign mandatory = "" -%}
-	{%- endif -%}
-	{%- if property.type.name != 'id' -%}
-	{{property.name}} {{property.type.name | upcase }} {{mandatory}}, 
-	{%- endif -%}
-	{% endfor %}
+		{%- liquid
+		    if property.maximum
+			assign data_size = property.maximum
+		    else
+			assign data_size = 255
+		    endif 
+		    if property.mandatory
+			 assign mandatory = " NOT NULL"
+		    else
+			 assign mandatory = ""
+		    endif 
+		   case property.type.name
+		      when "string"
+			 assign data_type = data_size | prepend: "VARCHAR(" | append: ")"
+		      when "text"
+			 assign data_type = "TEXT"
+		      when "integer"
+			 assign data_type = "INTEGER"
+		      else
+			 assign data_type = "???"
+		   endcase -%}
+		{%- if property.type.name != 'id' -%}
+			{{property.name}} {{data_type}}{{mandatory}}, 
+		{%- endif %}
+	{% endfor -%}
+
+	{% for property in object.properties %}
+		{%- if property.type.name == 'id' -%}
+			PRIMARY KEY ({{property.name}})
+		{%- endif -%}
+	{%- endfor %}
 );
+
+{% for index in object.indexes%}
+{% liquid
+   if index.unique
+	assign unicity = "UNIQUE "
+   else
+	assign unicity = ""
+   endif -%}
+CREATE {{unicity}}INDEX idx_{{index.name}} ON {{object.attributes.table_name}}(
+	{% for key in index.keys -%}
+	{%- liquid
+	  case key.type
+	     when "property"
+	        assign key_target = key.reference.name
+	     when "link"
+	        assign key_target = key.reference.name | capitalize | prepend: "id" 
+	  else
+	        assign key_target = "???"
+	  endcase
+	  if key.last
+		assign separator = ''
+	  else
+		assign separator = ','
+	  endif -%}
+	{{key_target}}{{separator}}
+	{% endfor -%}
+);
+{%- endfor %}
 {% endfor %}
+
+--------------------------------------------------------------------------------
+--     Foreign keys                                                           --
+--------------------------------------------------------------------------------
+
+{% for object in project.objects -%}
+{%- for link in object.links %}
+{%- assign target_table = link.target.attributes.table_name -%}
+ALTER TABLE {{object.attributes.table_name}} ADD CONSTRAINT fk_{{link.name}}
+	FOREIGN KEY (id{{link.name | capitalize}})
+	REFERENCES {{target_table}}(id);
+
+{%- endfor -%}
+{%- endfor %}
+
+--- end of sql script
